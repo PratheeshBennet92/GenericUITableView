de7d09e2503077fe90adaaa6f6a12309ee88a705
Pratheesh <pratheesh@Pratheeshs-MacBook-Pro.local>
2021-06-21 16:47:35 +0530

    Dev

--- GenericTableView/GenericTableView/ViewController.swift
+++ GenericTableView/GenericTableView/ViewController.swift
@@ -7,15 +7,18 @@ class ViewController: UIViewController {
   }()
   override func viewDidLoad() {
     super.viewDidLoad()
+    addListGrid()
     // Do any additional setup after loading the view.
   }
   private func addListGrid() {
+    self.listView.listDataSource?.dataSource = [ScheduleUIModel(id: 0, name: "Mr. Bean", type: "Comedy", language: "English", genres: "Comedy"),
+                                                ScheduleUIModel(id: 1, name: "Mr. Bean Animated Series", type: "Comedy", language: "English", genres: "Comedy")]
     listView.view.translatesAutoresizingMaskIntoConstraints  = false
     self.addChild(listView)
     self.view.addSubview(listView.view)
     listView.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
     listView.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
-    listView.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant:   Constants.hasTopNotch ? Constants.ListView.listViewTop : Constants.ListView.listViewTop).isActive = true
+    listView.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
     listView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
     listView.didMove(toParent: self)
   }
